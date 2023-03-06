315117a4340652d7a57ddc43807bdee3c051a288
LamberM <marcin.lamberski@gmail.com>
2023-03-06 12:06:03 +0100

    update UnitTest

--- src/test/java/org/LamberM/UnitTest.java
+++ src/test/java/org/LamberM/UnitTest.java
@@ -1,8 +1,9 @@
 package org.LamberM;
 
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@RunWith(MockitoJUnitRunner.class)
+
+@ExtendWith(MockitoExtension.class)
 public interface UnitTest {
 }
