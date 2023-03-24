d4ee7fc79b63435ad492ec8784a7244a1b837d9d
LamberM <marcin.lamberski@gmail.com>
2023-03-24 14:33:48 +0100

    to do

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,6 @@
-import org.LamberM.game.Game;
-
 public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello. This is RPG game");
-        Game game=new Game();
-        game.game();
-
     }
 }
