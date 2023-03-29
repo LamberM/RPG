d5e8dc586463035bb1a2d9d6d8f20d4dd4857049
LamberM <marcin.lamberski@gmail.com>
2023-03-29 19:10:06 +0200

    rebuild class

--- src/main/java/org/LamberM/game/Duel.java
+++ src/main/java/org/LamberM/game/Duel.java
@@ -1,107 +1,46 @@
 package org.LamberM.game;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.LamberM.character.Character;
-import org.LamberM.character.Enemy;
-import org.LamberM.character.MyClass;
-import org.LamberM.utils.MenuChooser;
-import org.LamberM.utils.SystemInReader;
 
-import java.util.List;
+import org.LamberM.character.Character;
 
 public class Duel {
 
-//    public Duel(Character hero , Character enemy)
-//    {
-//        hero = this.hero;
-//        enemy= this.enemy;
-//    }
-    @Getter
-    @Setter
-    private int range = 3;
-//    Character hero;
-//    Character enemy;
-    Round round = new Round();
-    MenuChooser duelMenuChooser= new MenuChooser(new SystemInReader(),
-            List.of(
-                    "1.Attack",
-                    "2.Offensive skills",
-                    "3.Defensive skills",
-                    "4.Step forward",
-                    "5.Rest"
-            )
-    );
+    private final Character enemy;
+    private final Character myHero;
 
-    private boolean heroCanUseSkill()
-    {
-        return (round.offensiveSkills() >= 0 && round.offensiveSkills() < 9999) || (round.defensiveSkills() >= 0 && round.defensiveSkills() <9999);
-    }
-    private void offensiveSkill()
-    {
-        round.offensiveSkills();
-        while (!heroCanUseSkill())
-        {
-            duelMenu();
-        }
-        round.enemyAttack();
-    }
-    private void defensiveSkill()
-    {
-        round.defensiveSkills();
-        while (!heroCanUseSkill())
-        {
-            duelMenu();
-        }
-        round.enemyAttack();
-    }
-    private void heroStatsInDuel()
-    {
-        System.out.println("My hero");
-        System.out.println("HP: " + round.myHero.duelStats.getDuelHP() + "MP: " + round.myHero.duelStats.getDuelMP());
+    public Duel(Character myHeroParameters, Character enemyParameters) {
+        this.myHero = myHeroParameters;
+        this.enemy = enemyParameters;
 
-    }
-    private void enemyStatsInDuel()
-    {
-        System.out.println("Enemy");
-        System.out.println("HP: " + round.enemy.duelStats.getDuelHP());
-    }
-    private boolean heroWin()
-    {
-        return round.enemy.duelStats.getDuelHP() <=0;
-    }
-    private boolean enemyWin()
-    {
-        return round.myHero.duelStats.getDuelHP() <=0;
     }
 
     public void duelMenu() {
-
+        int roundCounter = 0;
         while (!heroWin() || !enemyWin()) {
-            heroStatsInDuel();
-            enemyStatsInDuel();
-            int userChoice = duelMenuChooser.userPick();
-            switch (userChoice) {
-                case 1 -> {
-                    round.heroAttack();
-                    round.enemyAttack();
-                }
-                case 2 -> offensiveSkill();
-                case 3 -> defensiveSkill();
-                case 4 -> {
-                    range = range - 1;
-                    round.enemyAttack();
-                }
-                case 5 -> round.rest();
-            }
+            roundCounter++;
+            System.out.println("Round number " + roundCounter);
+            Round round = new Round(myHero, enemy, roundCounter);
+            round.playRound();
         }
-        if (enemyWin()){
+        if (enemyWin()) {
             System.out.println("You lost");
-        } else if (heroWin()) {
-            Journey journey = new Journey();
+            System.out.println("END GAME");
+        } else {
             System.out.println("You won");
+            Level level = new Level();
+            level.addExp();
+            Journey journey = new Journey(myHero);
             journey.mainMenu();
         }
     }
+
+    private boolean heroWin() {
+        return enemy.getDuelStats().getDuelHP() <= 0;
+    }
+
+    private boolean enemyWin() {
+        return myHero.getDuelStats().getDuelHP() <= 0;
+    }
+
 
 }
--- src/main/java/org/LamberM/game/Journey.java
+++ src/main/java/org/LamberM/game/Journey.java
@@ -1,6 +1,7 @@
 package org.LamberM.game;
 
-import org.LamberM.stats.AddStats;
+import org.LamberM.character.Character;
+import org.LamberM.stats.HeroAddStats;
 import org.LamberM.stats.ShowStats;
 import org.LamberM.utils.MenuChooser;
 import org.LamberM.utils.SystemInReader;
@@ -9,38 +10,46 @@ import java.util.List;
 
 public class Journey {
 
-    private final MenuChooser mainMenuChooser = new MenuChooser(new SystemInReader(),
-            List.of(
-                    "1.Fight",
-                    "2.Look at stats",
-                    "3.Add stats points",
-                    "4.Exit the game"
-            )
-    );
-    public CreateHero createHero = new CreateHero();
+    private final MenuChooser mainMenuChooser;
+    private final Character myHero;
+    private final int day;
 
+    public Journey(Character myHero) {
+        HeroCreator heroCreator = new HeroCreator();
+        myHero = heroCreator.createHero();
+        this.myHero = myHero;
+        day = 1;
+        mainMenuChooser = new MenuChooser(new SystemInReader(), List.of("1.Fight", "2.Look at stats", "3.Add stats points", "4.Exit the game"));
+    }
 
-    public void mainMenu()
-    {
-        System.out.println("Hello " + createHero.getName());
+    public void mainMenu() {
+        System.out.println("Hello " + myHero.getName());
         System.out.println("Are you ready for adventure ?");
         System.out.println("Your choice possibilities:");
         int userChoice = mainMenuChooser.userPick();
-        switch (userChoice)
-        {
+        switch (userChoice) {
             case 1 -> {
-                Duel duel  = new Duel();
+                EnemyCreator enemyCreator = new EnemyCreator();
+                Character enemy = enemyCreator.createEnemy();
+                Duel duel = new Duel(myHero, enemy);
                 duel.duelMenu();
             }
-            case 2 ->
-            {
-                ShowStats showStats = new ShowStats();
-                showStats.showStats();
+            case 2 -> {
+                ShowStats showStats = new ShowStats(myHero);
+                showStats.showStats(myHero);
             }
-            case 3 ->
-            {
-                AddStats addStats = new AddStats();
-                addStats.addStats();
+            case 3 -> {
+                Level level = new Level();
+                level.lvlUP();
+                if (!level.getLvlUp()) {
+                    System.out.println("You don't have level up");
+                } else {
+                    HeroAddStats heroAddStats = new HeroAddStats(myHero);
+                    while (!heroAddStats.currentPointsIsNull()) {
+                        heroAddStats.addStats();
+                    }
+                    level.setLvlToCompare(level.getLvl() + 1);
+                }
             }
             case 4 -> System.out.println("See you later, bye");
         }
