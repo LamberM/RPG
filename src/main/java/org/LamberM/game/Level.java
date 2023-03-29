16ac2b1b5a2165d07926e1e3b903be6f808b1a22
LamberM <marcin.lamberski@gmail.com>
2023-03-29 19:10:44 +0200

    little update class

--- src/main/java/org/LamberM/game/Level.java
+++ src/main/java/org/LamberM/game/Level.java
@@ -4,45 +4,47 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Random;
+
 @Getter
 @Setter
 public class Level {
-
     private int exp = 0;
 
     private int lvl = 1;
     private int lvlToCompare = 2;
-
-    public boolean getLvlUp()
-    {
-        return lvl == lvlToCompare;
-    }
-    private boolean giveLvlUp(){
-        return exp >=100;
-    }
-    private boolean expLessZero(){
-        return exp < 0;
-    }
-    private void expNotNegative()
-    {
-        if (expLessZero())
-        {
-            exp = 0;
-        }
-    }
-    public void lvlUP(){
+    public void lvlUP() {
         expNotNegative();
-        if (giveLvlUp()){
-            lvl = lvl+1;
-            exp= exp - 100;
+        if (giveLvlUp()) {
+            lvl = lvl + 1;
+            exp = exp - 100;
             System.out.println("Level up !!!");
             expNotNegative();
         }
     }
-    public void addExp()
-    {
+
+    public void addExp() {
         Random draw = new Random();
         exp = 70 + draw.nextInt(81);
     }
+
+
+    public boolean getLvlUp() {
+        return lvl == lvlToCompare;
+    }
+
+    private boolean giveLvlUp() {
+        return exp >= 100;
+    }
+
+    private boolean expLessZero() {
+        return exp < 0;
+    }
+
+    private void expNotNegative() {
+        if (expLessZero()) {
+            exp = 0;
+        }
+    }
+
 
 }
