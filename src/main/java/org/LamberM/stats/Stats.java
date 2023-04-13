a09bff6e5e7afc5bbcc48bfc0ec39d09b2486836
LamberM <marcin.lamberski@gmail.com>
2023-04-13 09:40:44 +0200

    delete duelHP and duel MP

--- src/main/java/org/LamberM/stats/Stats.java
+++ src/main/java/org/LamberM/stats/Stats.java
@@ -3,14 +3,11 @@ package org.LamberM.stats;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
 public class Stats {
-    public Stats(int strength, int dexterity, int intelligence, int hp, int mp, int dodge, int armor, int criticalChance, int attackRange)
-    {
+    public Stats(int strength, int dexterity, int intelligence, int hp, int mp, int dodge, int armor, int criticalChance, int attackRange) {
 //        if (!correctValues())
 //        {
 //            throw new IllegalArgumentException("Stats can't be less than 0");
@@ -20,17 +17,14 @@ public class Stats {
         this.intelligence = intelligence;
         this.hp = hp;
         this.mp = mp;
-        this.duelHP = hp;
-        this.duelMP= mp;
         this.dodge = dodge;
         this.armor = armor;
         this.criticalChance = criticalChance;
         this.attackRange = attackRange;
     }
+
     private int hp;
     private int mp;
-    private int duelHP;
-    private int duelMP;
     private int strength;
     private int dexterity;
     private int intelligence;
