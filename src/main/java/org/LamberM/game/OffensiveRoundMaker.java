5c0229ebef00b87c1422150b90fec02d5a1e2f43
LamberM <marcin.lamberski@gmail.com>
2023-05-09 13:22:14 +0200

    update

--- src/main/java/org/LamberM/game/OffensiveRoundMaker.java
+++ src/main/java/org/LamberM/game/OffensiveRoundMaker.java
@@ -3,37 +3,37 @@ package org.LamberM.game;
 
 import lombok.Setter;
 import org.LamberM.character.Character;
+import org.LamberM.utils.OutputWriter;
 import org.LamberM.utils.SystemOutWriter;
 
 import java.util.Map;
 import java.util.Random;
+import java.util.function.Consumer;
 
 public class OffensiveRoundMaker {
-    @Setter // for tests - setter method injection
-    private SystemOutWriter outWriter;
+
+    @Setter // - for tests - Setter Method Injection
+    private OutputWriter outputWriter;
     private int criticalChance;
     private int attackChance;
     private int dodgeChance;
 
     public OffensiveRoundMaker() {
-        this.outWriter = new SystemOutWriter();
+        outputWriter = new SystemOutWriter();
     }
 
     public void attack(Character attacker , Character defender) {
         attackOrCriticalOrMiss(attacker,defender);
         int attack = (20 + attacker.getDuelStats().getStrength() + attacker.getDuelStats().getDexterity()) - (defender.getDuelStats().getArmor() / 10);
         if (attackerMiss()) {
-            outWriter.setText("You missed");
-            outWriter.show();
+            outputWriter.show("You missed");
         } else {
             if (criticalAttack()) {
                 int critical = 2 * attack;
-                outWriter.setText("Critical attack for: " + critical + "!!!!!!!!!!!!");
-                outWriter.show();
+                outputWriter.show("Critical attack for: " + critical + "!!!!!!!!!!!!");
                 defender.getDuelStats().setHp(defender.getDuelStats().getHp() - critical);
             } else {
-                outWriter.setText("Attack for: " + attack);
-                outWriter.show();
+                outputWriter.show("Attack for: " + attack);
                 defender.getDuelStats().setHp(defender.getDuelStats().getHp() - attack);
             }
         }
@@ -43,25 +43,22 @@ public class OffensiveRoundMaker {
             Map<Integer, Runnable> offensiveSkillsMap = myHero.provideOffensiveSkills();
             Runnable offensiveSkill = offensiveSkillsMap.get(userChoice);
             if (offensiveSkill == null) {
-                outWriter.setText("Back to menu");
-                outWriter.show();
+                outputWriter.show("Back to menu");
             } else {
                 offensiveSkill.run();
                 attackOrCriticalOrMiss(myHero,enemy);
                 int attack = - (enemy.getDuelStats().getArmor() / 10);
                 if (attackerMiss()){
-                    outWriter.setText("You missed");
-                    outWriter.show();
+                    outputWriter.show("You missed");
                 }
                 else {
-                    if (!criticalAttack()){outWriter.setText("Attack for: " + attack);
-                        outWriter.show();
+                    if (!criticalAttack()){
+                        outputWriter.show("Attack for: " + attack);
                         enemy.getDuelStats().setHp(enemy.getDuelStats().getHp() - attack );
                     }
                     else {
                         int critical = 2 * attack;
-                        outWriter.setText("Critical attack for: " + critical + "!!!!!!!!!!!!");
-                        outWriter.show();
+                        outputWriter.show("Critical attack for: " + critical + "!!!!!!!!!!!!");
                         enemy.getDuelStats().setHp(enemy.getDuelStats().getHp() - critical );
                     }
                 }
--- src/test/java/org/LamberM/game/OffensiveRoundMakerTest.java
+++ src/test/java/org/LamberM/game/OffensiveRoundMakerTest.java
@@ -1,17 +1,27 @@
 package org.LamberM.game;
 
 import org.LamberM.UnitTest;
+import org.LamberM.character.Character;
 import org.LamberM.character.Enemy;
 import org.LamberM.character.Warrior;
+import org.LamberM.stats.Stats;
+import org.LamberM.utils.MenuChooser;
+import org.LamberM.utils.OutputWriter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.util.HashMap;
+
+import static org.mockito.Mockito.*;
 
 
 class OffensiveRoundMakerTest implements UnitTest {
     @InjectMocks
     OffensiveRoundMaker systemUnderTest;
-
+    @Mock
+    OutputWriter systemOutWriterMock;
     @Test
     void givenHeroAndEnemy_whenHeroAttack_thenDamage() {
         //given
@@ -39,15 +49,46 @@ class OffensiveRoundMakerTest implements UnitTest {
     }
 
     @Test
-    void givenHeroAndEnemy_whenOffensiveSkills_thenDamage() {
+    void givenOffensiveSkillsIsNotNull_whenOffensiveSkills_thenRunSkill(){
         //given
-        Warrior givenHero = new Warrior("does not matter");
-        Enemy givenEnemy = new Enemy("does not matter");
-        int enemyHpBeforeHeroAttack = givenEnemy.getDuelStats().getHp();
+        Character givenHero = mock(Character.class);
+        Character enemyMock = mock(Character.class);
+        MenuChooser menuChooserMock = mock(MenuChooser.class);
+        when(givenHero.provideOffensiveSkillsMenu()).thenReturn(menuChooserMock);
+
+        int userChoice = 1;
+        when(menuChooserMock.userPick()).thenReturn(userChoice);
+
+        HashMap<Integer, Runnable> offSkillsMap = new HashMap<>();
+        when(givenHero.provideOffensiveSkills()).thenReturn(offSkillsMap);
+
+        Runnable skillMock = mock(Runnable.class);
+        offSkillsMap.put(userChoice, skillMock);
+
+        Stats statsMock = mock(Stats.class);
+        when(givenHero.getDuelStats()).thenReturn(statsMock);
+        when(enemyMock.getDuelStats()).thenReturn(statsMock);
         //when
-        systemUnderTest.offensiveSkills(givenHero, givenEnemy);
+        systemUnderTest.offensiveSkills(givenHero,enemyMock);
         //then
-        int enemyHpAfterHeroAttack = givenEnemy.getDuelStats().getHp();
-        Assertions.assertTrue(enemyHpAfterHeroAttack <= enemyHpBeforeHeroAttack);
+        verify(skillMock).run();
+    }
+    @Test
+    void givenOffensiveSkillsIsNull_whenOffensiveSkills_thenDoNotRunSkill(){
+        //given
+        Character givenHero = mock(Character.class);
+        Character enemyMock = mock(Character.class);
+        MenuChooser menuChooserMock = mock(MenuChooser.class);
+        when(givenHero.provideOffensiveSkillsMenu()).thenReturn(menuChooserMock);
+
+        int userChoice = 1;
+        when(menuChooserMock.userPick()).thenReturn(userChoice);
+
+        String text = "Back to menu";
+        when(systemOutWriterMock.show(text)).thenReturn(text);
+        //when
+        systemUnderTest.offensiveSkills(givenHero,enemyMock);
+        //then
+        verify(systemOutWriterMock).show(text);
     }
 }
